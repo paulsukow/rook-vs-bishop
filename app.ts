a6ed453ae3858e8a7e0663a62f984d9cc33bb62f
paulsukow <21179028+paulsukow@users.noreply.github.com>
2022-05-18 19:20:46 -0600

    wip 2

--- app.ts
+++ app.ts
@@ -1,14 +1,14 @@
 /*
-* Assumptions:
-* When moving, pieces can pass thru others without triggering a collision.
-* Collisions are detected when the piece comes to rest.
+* Assumptions/Notes:
+* When moving, pieces can pass thru others without a collision.
 * According to the directions the rook only moves right, so don't worry about right to left or negative movements
 * Using 0 indexes for x and y direction
+* ChessBoard is static size
 */
 
 interface Board {
-  xValues: number[]
-  yValues: number[]
+  width: number
+  height: number
 }
 
 interface Position {
@@ -22,225 +22,127 @@ enum RookDirection {
 }
 
 class Rook {
-  public position: Position
+  private x: number
+  private y: number
   private board: Board
 
+  get position(): Position {
+    return { x: this.x, y: this.y }
+  }
+
   constructor(startingPosition: Position, board: Board) {
-    this.position = startingPosition
+    this.x = startingPosition.x
+    this.y = startingPosition.y
     this.board = board
   }
 
-  public movePiece(direction: RookDirection, distance: number): void {
+  public move(direction: RookDirection, distance: number): void {
     if (distance < 0) {
-      console.log('Distance must positive')
+      console.log('Invalid distance. Distance cannot be negative.')
       return
     }
 
     switch(direction) {
       case RookDirection.up: {
-        const dimensionSize = this.board.yValues.length
-        const y = (this.position.y + distance) % dimensionSize
-
-        this.position = {
-          ...this.position,
-          y
-        }
+        this.y = (this.y + distance) % this.board.height
+        this.logMove()
         break
       }
       case RookDirection.right: {
-        const dimensionSize = this.board.xValues.length
-        const x = (this.position.x + distance) % dimensionSize
-
-        this.position = {
-          ...this.position,
-          x
-        }
+        this.x = (this.x + distance) % this.board.width
+        this.logMove()
         break
       }
       default: {
-        console.log('Invalid direction')
+        console.log('Invalid direction. Cannot make move.')
         break
       }
     }
-    // console.log(`Rook moved to x: ${this.position.x}, y: ${this.position.y}`)
+  }
+
+  private logMove() {
+    console.log(`Rook moved to ${convertToFileRank(this.x, this.y)}.`)
   }
 }
 
 class Bishop {
-  private position: Position
-  private board: Board
+  private x: number
+  private y: number
 
-  constructor(startingPosition: Position, board: Board) {
-    this.position = startingPosition
-    this.board = board
+  constructor(startingPosition: Position) {
+    this.x = startingPosition.x
+    this.y = startingPosition.y
   }
 
-  public canCapturePieceAtPosition(otherPiecePosition: Position): boolean {
-    return Math.abs(otherPiecePosition.x - this.position.x) === Math.abs((otherPiecePosition.y - this.position.y))
+  public canCapturePieceAtPosition(position: Position): boolean {
+    return Math.abs(position.x - this.x) === Math.abs((position.y - this.y))
+  }
+}
+
+function convertToFileRank(x: number, y: number): string {
+  return `${String.fromCharCode(x + 65)}${y + 1}`
+}
+
+function convertFromFileRank(fileRank: string): Position {
+  return {
+    x: fileRank[0].toUpperCase().charCodeAt(0) - 65,
+    y: Number(fileRank[1]) - 1,
   }
 }
 
 function flipCoin(): number {
-  return generateBoundedRandomNumber(0, 1)
+  return generateRandomNumberInclusive(0, 1)
 }
 
 function rollDice(): number {
-  return generateBoundedRandomNumber(1, 6) + generateBoundedRandomNumber(1, 6)
+  return generateRandomNumberInclusive(1, 6) + generateRandomNumberInclusive(1, 6)
 }
 
-function generateBoundedRandomNumber(min: number, max: number) {
+function generateRandomNumberInclusive(min: number, max: number) {
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 const chessBoard: Board = {
-  xValues: [0, 1, 2, 3, 4, 5, 6, 7],
-  yValues: [0, 1, 2, 3, 4, 5, 6, 7],
+  width: 8,
+  height: 8,
 }
 
-const rookStartingPosition: Position = {
-  x: 7,
-  y: 0,
-}
+// const rookStartingPosition: Position = {
+//   x: 7,
+//   y: 0,
+// }
 
-const bishopStartingPosition: Position = {
-  x: 2,
-  y: 2,
-}
-
-const rook = new Rook(rookStartingPosition, chessBoard)
-const bishop = new Bishop(bishopStartingPosition, chessBoard)
-
-function moveRookAndGameStatus() {
-  const direction = flipCoin()
-  const distance = rollDice()
-  rook.movePiece(direction, distance)
-  return bishop.canCapturePieceAtPosition(rook.position)
-}
-
-const totalMoves = 15
 
 function playGame() {
-  for(let i = 0; i < totalMoves; i++) {
-    const a = moveRookAndGameStatus()
-    console.log('a')
-    if (a) {
-      return 'Bishop wins'
+  const rookStartingPosition: Position = convertFromFileRank('H1')
+  const rook = new Rook(rookStartingPosition, chessBoard)
+  console.log({ rookStartingPosition, expected: '7, 0'})
+
+  const bishopStartingPosition: Position = convertFromFileRank('C3')
+  const bishop = new Bishop(bishopStartingPosition)
+  console.log({ bishopStartingPosition, expected: '2,2'})
+
+  const totalMoves = 15
+
+  function performMoves() {
+    function moveRookAndGameStatus() {
+      const direction = flipCoin()
+      const distance = rollDice()
+      rook.move(direction, distance)
+      return bishop.canCapturePieceAtPosition(rook.position)
     }
+
+    for (let i = 0; i < totalMoves; i++) {
+      const status = moveRookAndGameStatus()
+      if (status) {
+        return 'Bishop wins'
+      }
+    }
+    return 'Rook wins'
   }
-  return 'Rook wins'
+
+  const result = performMoves()
+  console.log(result)
 }
 
-const result = playGame()
-console.log(result)
-
-
-
-
-
-
-
-
-
-
-//
-// if(!bishop.canCapturePieceAtPosition({ x: 0, y: 0 })) {
-//   console.log('wronge')
-// }
-//
-// if(!bishop.canCapturePieceAtPosition({ x: 1, y: 1 })) {
-//   console.log('wronge')
-// }
-//
-// if(!bishop.canCapturePieceAtPosition({ x: 2, y: 2 })) {
-//   console.log('wronge')
-// }
-//
-// if(!bishop.canCapturePieceAtPosition({ x: 3, y: 3 })) {
-//   console.log('wronge')
-// }
-//
-// if(!bishop.canCapturePieceAtPosition({ x: 7, y: 7 })) {
-//   console.log('wronge')
-// }
-//
-// if(!bishop.canCapturePieceAtPosition({ x: 0, y: 4 })) {
-//   console.log('wronge')
-// }
-//
-// if(!bishop.canCapturePieceAtPosition({ x: 1, y: 3 })) {
-//   console.log('wronge')
-// }
-//
-// if(!bishop.canCapturePieceAtPosition({ x: 2, y: 2 })) {
-//   console.log('wronge')
-// }
-//
-// if(!bishop.canCapturePieceAtPosition({ x: 3, y: 1 })) {
-//   console.log('wronge')
-// }
-// if(!'expected: true') {
-//   console.log('wronge')
-// }
-//
-// if(!bishop.canCapturePieceAtPosition({ x: 4, y: 0 })) {
-//   console.log('wronge')
-// }
-//
-//
-//
-// if(bishop.canCapturePieceAtPosition({ x: 0, y: 1 })) {
-//   console.log('noooo1')
-// }
-// if(bishop.canCapturePieceAtPosition({ x: 0, y: 2 })) {
-//   console.log('noooo2')
-// }
-// if(bishop.canCapturePieceAtPosition({ x: 0, y: 3 })) {
-//   console.log('noooo3')
-// }
-// // if(bishop.canCapturePieceAtPosition({ x: 0, y: 4 })) {
-// //   console.log('noooo4')
-// // }
-// if(bishop.canCapturePieceAtPosition({ x: 2, y: 1 })) {
-//   console.log('noooo5')
-// }
-// if(bishop.canCapturePieceAtPosition({ x: 2, y: 5 })) {
-//   console.log('noooo6')
-// }
-// if(bishop.canCapturePieceAtPosition({ x: 3, y: 5 })) {
-//   console.log('noooo7')
-// }
-// if(bishop.canCapturePieceAtPosition({ x: 3, y: 0 })) {
-//   console.log('noooo8')
-// }
-
-
-
-
-
-
-// console.log(RookDirection[tossCoin()])
-// console.log(RookDirection[tossCoin()])
-// console.log(RookDirection[tossCoin()])
-// console.log(RookDirection[tossCoin()])
-// console.log(RookDirection[tossCoin()])
-//
-// rook.movePiece(RookDirection.up, 1)
-// console.log('expected y: 1')
-//
-// rook.movePiece(RookDirection.up, 4)
-// console.log('expected y: 5')
-//
-// rook.movePiece(RookDirection.up, 10)
-// console.log('expected y: 7')
-//
-//
-// rook.movePiece(RookDirection.right, 1)
-// console.log('expected x: 0')
-//
-// rook.movePiece(RookDirection.right, 4)
-// console.log('expected x: 4')
-//
-// rook.movePiece(RookDirection.right, 10)
-// console.log('expected x: 6')
-
-
+playGame()
